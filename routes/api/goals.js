bff4fa78b4bf676a97308730b96a1751ca7a432f
j3nhan <j3nnynhan@gmail.com>
2021-08-16 16:43:33 -0700

    work on goals routes

--- routes/api/goals.js
+++ routes/api/goals.js
@@ -3,3 +3,24 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+const testing_goals = [
+    {
+        id: 'g1',
+        type: 'building habit',
+        title: 'testing goals',
+        description: 'making sure it works',
+        author: 'a1'
+    }
+];
+
+router.get('/', (req, res, next) => {
+    const goalId = req.params.pid;
+    const goal = testing_goals.find( g => {
+        return g.id === goalId;
+    });
+
+    res.json({ goal });
+})
+
+
+module.exports = router;
