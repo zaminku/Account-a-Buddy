8e7c62649f143f3871934346e6920705fbfd96e3
b-chai <b.chai82@gmail.com>
2021-08-16 19:42:44 -0400

    add frontpage elements

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -5,7 +5,10 @@ class MainPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>Account-a-Buddy</h1>
+                <h1> Account-a-Buddy </h1>
+                <div> Catchphrase </div>
+                <div> Description </div>
+                <button>Get Started</button>
             </div>
         );
     }
