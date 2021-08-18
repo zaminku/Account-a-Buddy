7e1e20b7c75133037ff3a6e04099fe02e516446d
j3nhan <j3nnynhan@gmail.com>
2021-08-18 11:30:13 -0700

    work on goal index container

--- frontend/src/components/goals/goal_index_container.js
+++ frontend/src/components/goals/goal_index_container.js
@@ -1,12 +1,13 @@
 import { connect } from "react-redux";
+import { fetchGoals } from '../../actions/goal_actions';
 import GoalIndex from "./goal_index";
 
 const mSTP = state => ({
-
+    goals: Object.values(state.goals.all) //change .all
 })
 
 const mDTP = dispatch => ({
-
+    fetchGoals: () => dispatch(fetchGoals());
 })
 
 export default connect(mSTP, mDTP)(GoalIndex);
