3e01c855ec84dfa6ac399700e5bbc4e39929641a
b-chai <b.chai82@gmail.com>
2021-08-18 20:44:15 -0400

    add team images

--- frontend/src/components/contact/contact_us.jsx
+++ frontend/src/components/contact/contact_us.jsx
@@ -8,39 +8,41 @@ function ContactPage() {
             <div></div>
 
             <div>Meet the Team</div>
-            <ul>
+
+            <ul className="team">
+
                 <li>Zamin
-                    <div className="Zamin"></div>
-                    <div>
-                        Links :
-                    <a href="https://github.com/zaminku"><img src="../github-logo.png" alt="Github-logo" /></a>
-                    <a href=""><img src="../linkedin-logo.png" alt="LinkedIn-Logo" /></a>
-                    </div>
-                </li>
-                <li>Sam
-                    <div className="Sam"></div>
-                    <div>
-                        Links :
-                    <a href="https://github.com/samsongs1991"><img src="../github-logo.png" alt="Github-logo" /></a>
-                    <a href=""><img src="../linkedin-logo.png" alt="LinkedIn-Logo" /></a>
-                    </div>
-                </li>
-                <li>Jenny
-                    <div className="Jenny"></div>
-                    <div>
-                        Links :
-                    <a href="https://github.com/j3nhan"><img src="../github-logo.png" alt="Github-logo" /></a>
-                    <a href=""><img src="../linkedin-logo.png" alt="LinkedIn-Logo" /></a>
-                    </div>
-                </li>
-                <li>Ben
-                    <div className="Ben"></div>
+                    <div className="Zamin"><img src="../zamin.jpeg" alt="user"/></div>
                     <div className="links">
-                        Links :
-                    <a href="https://github.com/b-chai"><img src="../github-logo.png" alt="Github-logo" /></a>
-                    <a href="https://www.linkedin.com/in/ben-chai/"><img src="../linkedin-logo.png" alt="LinkedIn-Logo" /></a>
+                    <a href="https://github.com/zaminku"><img className="github" src="../github-logo.png" alt="Github-logo" /></a>
+                    <a href=""><img className="linkedin" src="../linkedin-logo.png" alt="LinkedIn-Logo" /></a>
                     </div>
                 </li>
+
+                <li>Sam
+                    <div className="Sam"><img src="../sam.jpg" alt="user"/></div>
+                    <div className="links">
+                    <a href="https://github.com/samsongs1991"><img className="github" src="../github-logo.png" alt="Github-logo" /></a>
+                    <a href=""><img className="linkedin" src="../linkedin-logo.png" alt="LinkedIn-Logo" /></a>
+                    </div>
+                </li>
+
+                <li>Jenny
+                    <div className="Jenny"><img src="../jenny.jpeg" alt="user"/></div>
+                    <div className="links">
+                    <a href="https://github.com/j3nhan"><img className="github" src="../github-logo.png" alt="Github-logo" /></a>
+                    <a href=""><img className="linkedin" src="../linkedin-logo.png" alt="LinkedIn-Logo" /></a>
+                    </div>
+                </li>
+
+                <li>Ben
+                    <div className="Ben"><img src="../ben.jpeg" alt="user"/></div>
+                    <div className="links">
+                    <a href="https://github.com/b-chai"><img className="github" src="../github-logo.png" alt="Github-logo" /></a>
+                    <a href="https://www.linkedin.com/in/ben-chai/"><img className="linkedin" src="../linkedin-logo.png" alt="LinkedIn-Logo" /></a>
+                    </div>
+                </li>
+                
             </ul>
         </div>
     )
