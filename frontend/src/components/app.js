597efe5964a3de1d6c21d4e65703b6952ee9e117
j3nhan <j3nnynhan@gmail.com>
2021-08-21 15:25:06 -0700

    add goal list container route

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,6 +7,7 @@ import MainPage from './main/main_page.jsx';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import GoalIndexContainer from './goals/goal_index_container';
+import GoalListContainer from './goals/goal_list_container';
 import GoalFormContainer from './goals/goal_form_container';
 import MessageIndexContainer from './messages/message_index_container';
 // import ChatPageContainer from './messages/chat_page_container';
@@ -23,6 +24,7 @@ const App = () => (
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
             <ProtectedRoute exact path="/goals" component={GoalIndexContainer}/>
+            <ProtectedRoute exact path="/goalList" component={GoalListContainer}/>
             <ProtectedRoute exact path="/goals/new" component={GoalFormContainer}/>
             <ProtectedRoute path="/chat" component={MessageIndexContainer}/>
         </Switch>
---
+++ frontend/src/components/goals/goal_list.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+
+class GoalList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+
+}
+
+export default GoalList;
